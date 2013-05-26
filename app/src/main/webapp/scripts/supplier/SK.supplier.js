d9929cc9235e1c8ca7c566a686ff5d4b06b12876
Vincent Kellers da Silveira <kellers.vincent@gmail.com>
2013-05-26 19:51:54 -0300

    Adicionando modulo de fornecedores

---
+++ app/src/main/webapp/scripts/supplier/SK.supplier.js
@@ -0,0 +1,35 @@
+if (SK === undefined) {
+   var SK = {};
+}
+
+SK.supplier = function(userOptions) {
+
+	var that = {};
+	
+	var crud = new FW.crud({
+		module: 'supplier',
+		key: 'supplierList',
+		deleteSuccessMessage: 'Fornecedor excluído com sucesso.',
+		saveSuccessMessage: 'Fornecedor atualizado com sucesso.',
+		saveValidation: function(object) {
+			var result = '';
+			
+			if (object.name == '') {
+				result = 'Informe o nome do fornecedor.';
+			}
+			
+			if (object.cell == '' && object.phone == '') {
+				result += ' Informe ao menos um telefone de contato do fornecedor.';
+			}
+			
+			return result;
+		}
+	});
+
+	that.load = function() {
+		crud.init();
+	}
+	
+	return that;
+}
+	
